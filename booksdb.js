cae2df1cec408c408d772b9dbdf9163ffed78d50
Phu Nguyen <90121579+PhuNguyen398003@users.noreply.github.com>
2023-08-14 13:14:47 +0700

    Update booksdb.js

--- booksdb.js
+++ booksdb.js
@@ -1,5 +1,5 @@
 
-{ books : {
+{
       "1": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       "2": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       "3": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
@@ -10,4 +10,4 @@
       "8": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
       "9": {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
       "10": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}}
+}
