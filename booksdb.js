7cc08caa85c1ffdc4fe08dce506b18d4241c7fa1
Phu Nguyen <90121579+PhuNguyen398003@users.noreply.github.com>
2023-08-14 16:20:02 +0700

    Update booksdb.js

--- booksdb.js
+++ booksdb.js
@@ -1,14 +1,18 @@
 
-{ "books" :{
-      "1": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      "2": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      "3": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      "4": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      "5": {"author": "Unknown","title": "The Book Of Job", "reviews": {"test" : "abc"} },
-      "6": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      "7": {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      "8": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      "9": {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      "10": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+{     
+      "mark01" : {
+            "1": "mark01_01_data"
+      } ,
+      "books" :{
+            "1": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+            "2": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+            "3": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+            "4": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+            "5": {"author": "Unknown","title": "The Book Of Job", "reviews": {"test" : "abc"} },
+            "6": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+            "7": {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+            "8": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+            "9": {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+            "10": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
       }
 }
